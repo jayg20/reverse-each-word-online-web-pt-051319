2dae4701773e7453c6a78874028b6277ef719819
James Gomez <james.gomez320@yahoo.com>
2019-05-23 05:36:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,17 @@
-def reverse_each_word(string)
-seperates_words = string.split("")
+def reverse_each_word(sentence)
+seperates_words = sentence.split("")
 array =[]
-seperates_words.each do |string|
-  array << string.reverse
+seperates_words.each do |sentence|
+  array << sentence.reverse
 end
 return array.join("")
 end
 
-reverse_each_word(string)
-seperates_words_one = string.split("")
+reverse_each_word(sentence)
+seperates_words_one = sentence.split("")
 new_array = []
-seperates_words_one.collect do |string|
-  new_array << string.reverse
+seperates_words_one.collect do |sentence|
+  new_array << sentence.reverse
 end
 new_array.join("")
 end 
